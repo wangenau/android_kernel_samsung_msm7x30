4ad9778e76d77522671f3d9d58e9c8ee329f2448
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-05-07 07:44:34 +0200

    ariesve: High memory configurations

--- arch/arm/mach-msm/board-ariesve.c
+++ arch/arm/mach-msm/board-ariesve.c
@@ -149,7 +149,24 @@ EXPORT_SYMBOL(sec_class);
 struct device *switch_dev;
 EXPORT_SYMBOL(switch_dev);
 
-#define MSM_PMEM_SF_SIZE	0x1A00000
+#ifdef CONFIG_MSM_MEMORY_HIGH               // 388 MB of free RAM
+#define MSM_PMEM_SF_SIZE          0x1200000 //    18.874.368 Bytes =  18 MB
+#define MSM_PMEM_ADSP_SIZE        0x1200000 //    18.874.368 Bytes =  18 MB
+#elif defined(CONFIG_MSM_MEMORY_VERY_HIGH)  // 398 MB of free RAM
+#define MSM_PMEM_SF_SIZE          0x0800000 //     8.388.608 Bytes =   8 MB
+#define MSM_PMEM_ADSP_SIZE        0x1200000 //    18.874.368 Bytes =  18 MB
+#elif defined(CONFIG_MSM_MEMORY_EXTRA_HIGH) // 406 MB of free RAM
+#define MSM_PMEM_SF_SIZE          0x0800000 //     8.388.608 Bytes =   8 MB
+#define MSM_PMEM_ADSP_SIZE        0x0A00000 //    10.485.760 Bytes =  10 MB
+#else                                       // 380 MB of free RAM
+#define MSM_PMEM_SF_SIZE          0x1A00000 //    27.262.976 Bytes =  26 MB
+#define MSM_PMEM_ADSP_SIZE        0x1200000 //    18.874.368 Bytes =  18 MB
+#endif
+
+#define MSM_FLUID_PMEM_ADSP_SIZE  0x2800000 //    41.943.040 Bytes =  40 MB
+#define PMEM_KERNEL_EBI0_SIZE     0x0600000 //     6.291.456 Bytes =   6 MB
+#define MSM_PMEM_AUDIO_SIZE       0x0200000 //     2.097.152 Bytes =   2 MB
+
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MSM_FB_PRIM_BUF_SIZE	(roundup((800 * 480 * 4), 4096) * 3) /* 4bpp * 3 Pages */
 #else
@@ -157,11 +174,6 @@ EXPORT_SYMBOL(switch_dev);
 #endif
 
 #define MSM_FB_SIZE roundup(MSM_FB_PRIM_BUF_SIZE, 4096)
-
-#define MSM_PMEM_ADSP_SIZE		0x1200000
-#define MSM_FLUID_PMEM_ADSP_SIZE	0x2800000
-#define PMEM_KERNEL_EBI0_SIZE		0x0600000
-#define MSM_PMEM_AUDIO_SIZE		0x0200000
 
 #ifdef CONFIG_ION_MSM
 static struct platform_device ion_dev;
