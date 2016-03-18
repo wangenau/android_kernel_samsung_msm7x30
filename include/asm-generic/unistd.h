b50ca109f9a2c447dbc4b62a4e3e18e610c7b002
sirmordred <bambu_bambu@windowslive.com>
2016-03-18 18:41:58 +0200

    kernel: Fix finit_module support * Many thanx to arco for his help

--- include/asm-generic/unistd.h
+++ include/asm-generic/unistd.h
@@ -695,7 +695,7 @@ __SC_COMP(__NR_process_vm_writev, sys_process_vm_writev, \
 __SYSCALL(__NR_finit_module, sys_finit_module)
 
 #undef __NR_syscalls
-#define __NR_syscalls 272
+#define __NR_syscalls 274
 
 /*
  * All syscalls below here should go away really,
